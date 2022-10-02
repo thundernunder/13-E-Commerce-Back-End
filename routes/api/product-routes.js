73d48b19a458403772db90626cebbb5175850efb
Nunder <nathannunderwood@gmail.com>
2022-10-01 22:28:10 -0700

    Start build product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,20 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category, 
+        attributes: ['id', 'category_name']
+      }, 
+
+      {
+        model: Tag, 
+        attributes: ['id', 'tag_name']
+      }
+      
+    ]
+  })
 });
 
 // get one product
